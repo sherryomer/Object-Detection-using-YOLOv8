35ae7b79b813e079c128f44689dda17251317c2a
sherryomer <99289577+sherryomer@users.noreply.github.com>
2025-03-08 05:44:50 +0500

    Add files via upload

---
+++ vid2.py
@@ -0,0 +1,6 @@
+from ultralytics import YOLO
+import cv2
+model = YOLO('bt.pt')
+cap = cv2.VideoCapture(0) 
+model.predict(source='0',show=True, conf = 0.5)
+cap = cv2.VideoCapture(0, apiPreference=cv2.CAP_AVFOUNDATION)
